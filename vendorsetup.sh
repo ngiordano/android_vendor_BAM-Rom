0e9017d7d19d00bf063968fe26e349603213bf3d
iGio90 <giovanni.rocca.90@gmail.com>
2013-08-22 17:37:03 +0200

    Track jbam10 Signed-off-by: iGio90 <giovanni.rocca.90@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl https://raw.github.com/iGio90/jellybam_vendor/jbam9/bam-build-targets | sed -e 's/#.*$//' | grep bam9 | awk {'print $1'})
+for combo in $(curl https://raw.github.com/iGio90/jellybam_vendor/jbam10/bam-build-targets | sed -e 's/#.*$//' | grep jbam10 | awk {'print $1'})
 do
     add_lunch_combo $combo
 done
