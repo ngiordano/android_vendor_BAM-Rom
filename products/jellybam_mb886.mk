1f20abdb141b256804a62929c53e936fa5c4964d
iGio90 <giovanni.rocca.90@gmail.com>
2013-09-08 13:54:38 +0200

    Fix mb886 path and pyramid

--- products/jellybam_mb886.mk
+++ products/jellybam_mb886.mk
@@ -11,7 +11,7 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/jellybam/overlay/aokp/device/common
 include vendor/jellybam/config/jellybam_common.mk
 
 # Inherit CM device configuration
-$(call inherit-product, device/moto/mb886/cm.mk)
+$(call inherit-product, device/motorola/mb886/cm.mk)
 
 PRODUCT_NAME := jellybam_mb886
 
