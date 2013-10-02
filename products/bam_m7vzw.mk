da7d5c5d52b6f3f5b5e8c73ab43990f4885c91ad
iGio90 <giovanni.rocca.90@gmail.com>
2013-10-02 06:54:31 +0200

    m7vzw and t769

---
+++ products/bam_m7vzw.mk
@@ -0,0 +1,23 @@
+# Check for target product
+ifeq (bam_m7vzw,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_xxhdpi
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/bam/overlay/aokp/device/common
+
+# include JELLYBAM common configuration
+include vendor/bam/config/bam_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/htc/m7vzw/cm.mk)
+
+PRODUCT_NAME := bam_m7vzw
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/bam/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/bam/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
+
