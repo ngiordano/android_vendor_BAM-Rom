e84041aefbba428fb376308dd7e74e6da0e18c6e
Giovanni Rocca <giovanni.rocca.90@gmail.com>
2013-10-11 01:30:23 +0200

    added venturi: Galaxy Player 5

---
+++ products/bam_venturi_usa.mk
@@ -0,0 +1,23 @@
+# Check for target product
+ifeq (bam_venturi_usa,$(TARGET_PRODUCT))
+
+# OVERLAY_TARGET adds overlay asset source
+OVERLAY_TARGET := pa_hdpi
+
+# AOKP device overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/bam/overlay/aokp/device/common
+
+# include JELLYBAM common configuration
+include vendor/bam/config/bam_common.mk
+
+# Inherit CM device configuration
+$(call inherit-product, device/samsung/venturi_usa/cm.mk)
+
+PRODUCT_NAME := bam_venturi_usa
+
+# Update local_manifest.xml
+GET_PROJECT_RMS := $(shell vendor/bam/tools/removeprojects.py $(PRODUCT_NAME))
+GET_PROJECT_ADDS := $(shell vendor/bam/tools/addprojects.py $(PRODUCT_NAME))
+
+endif
+
