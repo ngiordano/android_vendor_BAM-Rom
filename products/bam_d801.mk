a2b41b1d75497b6855a0b91f57cd08e06bdf6f03
iGio90 <giovanni.rocca.90@gmail.com>
2013-12-16 12:10:03 +0100

    d800, d801, d802

--- products/bam_d801.mk
+++ products/bam_d801.mk
@@ -1,5 +1,5 @@
 # Check for target product
-ifeq (bam_d800,$(TARGET_PRODUCT))
+ifeq (bam_d801,$(TARGET_PRODUCT))
 
 # OVERLAY_TARGET adds overlay asset source
 OVERLAY_TARGET := pa_xxhdpi
@@ -11,9 +11,9 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/bam/overlay/aokp/common
 include vendor/bam/config/bam_common.mk
 
 # Inherit CM device configuration
-$(call inherit-product, device/lge/d800/cm.mk)
+$(call inherit-product, device/lge/d801/cm.mk)
 
-PRODUCT_NAME := bam_d800
+PRODUCT_NAME := bam_d801
 
 # Update local_manifest.xml
 GET_PROJECT_RMS := $(shell vendor/bam/tools/removeprojects.py $(PRODUCT_NAME))
